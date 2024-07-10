f74ae91fa6ca6db111236557ba0c8f9c2622757a
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-07-10 00:01:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ensure the audio file is loaded correctly
+let sound1 = document.getElementById("sound1");
+let sound2 = document.getElementById("sound2");
+let sound3 = document.getElementById("sound3");
+let sound4 = document.getElementById("sound4");
